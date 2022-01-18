1e508d3c414cb2d0324fcd6276a5565bbc5e9ac8
freebee555 <nativity+github@gmail.com>
2022-01-18 15:03:23 +0800

    可变参数练习

--- 1.py
+++ 1.py
@@ -1,3 +1,19 @@
-a='dfadfasdfadf'
-b='adfasdf13134'
-print(a,b)
+from lib2to3.pgen2.token import LBRACE
+
+
+a='1234'
+b='98934'
+
+la=len(a)
+lb=len(b)
+
+if la>lb:
+    b='0'*(la-lb)+b
+    m=la
+else:
+    a='0'*(lb-la)+a
+    m=la
+
+na=[int(a[i]) for i in range(m)]
+nb=[int(b[i]) for i in range(m)]
+print(na,nb)
